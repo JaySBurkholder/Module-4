1e470d96399dd7035a39aa890424814739f60421
J Burkholder <Jay.S.Burkholder@gmail.com>
2022-08-31 20:34:25 -0400

    committing js for questions

--- CodingQuiz.js
+++ CodingQuiz.js
@@ -1,38 +1,51 @@
 // button when pressed needs to bring up questions in the html that ask about code and then have four clickable options
 // start quiz code here
+let questionDiv = document.getElementById("Question")
+let answers1 = document.getElementById("answers1")
+let answers2 = document.getElementById("answers2")
+let answers3 = document.getElementById("answers3")
+let answers4 = document.getElementById("answers4")
+
 let button = document.getElementById("button1")
+// my start quiz button getting a variable here
 let timeholder = document.getElementById("timer")
+// timer getting variable
 let timeleft = 60
+// timer starts at 60
 let Questions = [
+    // questions array with answers
     {question: "What is a Function?",
-    answers: {
-        a: "a math thingy",
-        b: "right answer",
-        c: "dhlsfjd",
-        d: "asdddad",
-    },
+    answers: [
+        "a math thingy",
+        "right answer",
+        "dhlsfjd",
+        "asdddad",
+    ],
     correctAnswer: "right answer",
-    },
-    {question: "What is a Function?",
-    answers: {
-        a: "a math thingy",
-        b: "right answer",
-        c: "dhlsfjd",
-        d: "asdddad",
-    },
-    correctAnswer: "right answer",
-    },
-    {question: "What is a Function?",
-    answers: {
-        a: "a math thingy",
-        b: "right answer",
-        c: "dhlsfjd",
-        d: "asdddad",
-    },
-    correctAnswer: "right answer",
-    },
+},
+{question: "What is an Object?",
+answers: [
+    "a math thingy",
+    "right answer",
+    "dhlsfjd",
+    "asdddad",
+],
+correctAnswer: "right answer",
+},
+{question: "What is a code?",
+answers: [
+    "a math thingy",
+    "right answer",
+    "dhlsfjd",
+    "asdddad",
+],
+correctAnswer: "right answer",
+}
 ]
 
+
+
+// function for timer
 function startTimer() {
     let Timer = setInterval(function() {
     if(timeleft <= 0) {
@@ -48,12 +61,19 @@ function createQuestions(question) {
 }
 
 button.addEventListener("click", function() {
+// start quiz button is clickable and on click executes function
    var QuestionDiv = document.getElementById("Question")
+//    question div appears on click and puts question to dom
     if (QuestionDiv.classList) {
+        // returns CSS classnames of elements 
         console.log("this.addEventListener ran")
+        // making sure it ran
         QuestionDiv.classList = [];
+        // store class names in an array
     }
     button.classList.add("hide")
+    // add element with class of hide after button is clicked
     startTimer()
+    // start timer next
 })
 
